0f166929a6be316c858f7f1a0cc3147a8b8233a4
zlcython-by-zlc1003 <107649766+zlcython-by-zlc1003@users.noreply.github.com>
2024-11-20 22:43:37 -0800

    fix ip error

--- app.py
+++ app.py
@@ -7,87 +7,118 @@ import requests
 import time
 import os
 
-webhook=os.getenv("MAIN_WEBHOOK")
-txidWebhook=os.getenv("TXID_WEBHOOK")
+webhook = os.getenv("MAIN_WEBHOOK")
+txidWebhook = os.getenv("TXID_WEBHOOK")
 
 app = flask.Flask(__name__, static_folder="public", static_url_path="")
 
-def sendWebhook(msg,webhook=webhook):
-    return requests.post(webhook,json={"content":msg})
+
+def sendWebhook(msg, webhook=webhook):
+    return requests.post(webhook, json={"content": msg})
+
 
 def timeStr():
-    return "<t:"+str(int(time.time()))+">"
+    return "<t:" + str(int(time.time())) + ">"
 
-def log(requestType,msg,ip):
-    out=""
-    out+=timeStr()
-    out+=" `"
-    out+=requestType
-    out+="` `"
-    out+=ip
-    out+="` `"
-    out+=msg
-    out+="`"
+
+def log(requestType, msg, ip):
+    out = ""
+    out += timeStr()
+    out += " `"
+    out += requestType
+    out += "` `"
+    out += ip
+    out += "` `"
+    out += msg
+    out += "`"
     sendWebhook(out)
 
-def logTxid(txid,ip):
-    out=""
-    out+=timeStr()
-    out+=" `"
-    out+="TXID"
-    out+="` `"
-    out+=txid
-    out+="` `"
-    out+=ip
-    out+="`"
-    sendWebhook(out,txidWebhook)
 
-def validateToken(username,ip,token)->bool:
-    return token == (hashlib.sha256((username+","+getUsers().get(username,"")+","+ip).encode()).hexdigest())
+def logTxid(txid, ip):
+    out = ""
+    out += timeStr()
+    out += " `"
+    out += "TXID"
+    out += "` `"
+    out += txid
+    out += "` `"
+    out += ip
+    out += "`"
+    sendWebhook(out, txidWebhook)
 
-def getToken(username,password,ip)->str:
-    return hashlib.sha256((username+","+password+","+ip).encode()).hexdigest()
+
+def validateToken(username, ip, token) -> bool:
+    return token == (
+        hashlib.sha256(
+            (username + "," + getUsers().get(username, "") + "," + ip).encode()
+        ).hexdigest()
+    )
+
+
+def getToken(username, password, ip) -> str:
+    return hashlib.sha256((username + "," + password + "," + ip).encode()).hexdigest()
+
 
 def getUsers() -> dict:
     with open("priv/userpass.json", "r") as f:
         return json.load(f)
+
+
 def getAddress() -> dict:
     with open("priv/address.json", "r") as f:
         return json.load(f)
 
-def getAmount(coin,invoiceType) -> dict:
+
+def getAmount(coin, invoiceType) -> dict:
     with open("priv/amount.json", "r") as f:
-        return json.load(f)[coin+str(invoiceType)]
+        return json.load(f)[coin + str(invoiceType)]
+
 
 def serve_qr(text):
     pil_img = qrcode.make(text)
     img_io = io.BytesIO()
-    pil_img.save(img_io, 'JPEG')
+    pil_img.save(img_io, "JPEG")
     img_io.seek(0)
-    return flask.send_file(img_io, mimetype='image/jpeg')
+    return flask.send_file(img_io, mimetype="image/jpeg")
+
 
 def addTxid(txid):
-    with open("priv/txid.txt","a") as f:
-        f.write(txid+"\n")
+    with open("priv/txid.txt", "a") as f:
+        f.write(txid + "\n")
+
+
+def getIP(req: flask.Request) -> str:
+    if req.headers.get("X-Forwarded-For", False):
+        return req.headers.get("X-Forwarded-For")
+    return req.remote_addr
+
 
 @app.route("/")
 def index():
-    log(flask.request.method,flask.request.full_path,flask.request.remote_addr)
+    log(flask.request.method, getIP(flask.request), flask.request.remote_addr)
     return flask.send_from_directory("static", "index.html")
 
 
 @app.route("/deposit")
 def deposit():
-    log(flask.request.method,flask.request.full_path,flask.request.remote_addr)
-    if not validateToken(flask.request.cookies.get("username",""),flask.request.remote_addr,flask.request.cookies.get("token")):
+    log(flask.request.method, getIP(flask.request), flask.request.remote_addr)
+    if not validateToken(
+        flask.request.cookies.get("username", ""),
+        flask.request.remote_addr,
+        flask.request.cookies.get("token"),
+    ):
         return flask.redirect("/login")
     return flask.send_from_directory("static", "deposit.html")
 
 
 @app.route("/withdraw", methods=["GET", "POST"])
 def withdraw():
-    log(flask.request.method,flask.request.full_path,flask.request.remote_addr)
-    if not validateToken(flask.request.cookies.get("username",""),flask.request.remote_addr,flask.request.cookies.get("token")):
+    log(flask.request.method,getIP(flask.request),flask.request.remote_addr)
+    if not validateToken(
+        flask.request.cookies.get("username", ""),
+        flask.request.remote_addr,
+        flask.request.cookies.get("token"),
+    ):
         return flask.redirect("/login")
     if flask.request.method == "GET":
         return flask.send_from_directory("static", "withdraw.html")
@@ -97,84 +128,130 @@ def withdraw():
 
 @app.route("/upgrade")
 def upgrade():
-    log(flask.request.method,flask.request.full_path,flask.request.remote_addr)
-    if not validateToken(flask.request.cookies.get("username",""),flask.request.remote_addr,flask.request.cookies.get("token")):
+    log(flask.request.method,getIP(flask.request),flask.request.remote_addr)
+    if not validateToken(
+        flask.request.cookies.get("username", ""),
+        flask.request.remote_addr,
+        flask.request.cookies.get("token"),
+    ):
         return flask.redirect("/login")
     return flask.send_from_directory("static", "upgrade.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    log(flask.request.method,flask.request.full_path,flask.request.remote_addr)
+    log(flask.request.method,getIP(flask.request),flask.request.remote_addr)
     if flask.request.method == "POST":
         username = flask.request.form["username"]
         password = flask.request.form["password"]
         if getUsers().get(username, False) == password:
             resp = flask.make_response(flask.redirect("/home"))
             resp.set_cookie("username", username)
-            resp.set_cookie("token", getToken(username,password,flask.request.remote_addr))
-            log("INFO","Login Successful, user: "+username+", pass:"+password,flask.request.remote_addr)
+            resp.set_cookie(
+                "token", getToken(username, password, flask.request.remote_addr)
+            )
+            log(
+                "INFO",
+                "Login Successful, user: " + username + ", pass:" + password,
+                flask.request.remote_addr,
+            )
             return resp
         else:
             return flask.send_from_directory("static", "loginerror.html")
     elif flask.request.method == "GET":
-        if validateToken(flask.request.cookies.get("username",""),flask.request.remote_addr,flask.request.cookies.get("token")):
+        if validateToken(
+            flask.request.cookies.get("username", ""),
+            flask.request.remote_addr,
+            flask.request.cookies.get("token"),
+        ):
             return flask.redirect("/home")
         return flask.send_from_directory("static", "login.html")
 
 
 @app.route("/home")
 def home():
-    log(flask.request.method,flask.request.full_path,flask.request.remote_addr)
-    if not validateToken(flask.request.cookies.get("username",""),flask.request.remote_addr,flask.request.cookies.get("token")):
+    log(flask.request.method,getIP(flask.request),flask.request.remote_addr)
+    if not validateToken(
+        flask.request.cookies.get("username", ""),
+        flask.request.remote_addr,
+        flask.request.cookies.get("token"),
+    ):
         return flask.redirect("/login")
     return flask.send_from_directory("static", "home.html")
 
+
 @app.route("/forgot")
 def forgot():
-    log(flask.request.method,flask.request.full_path,flask.request.remote_addr)
-    if not validateToken(flask.request.cookies.get("username",""),flask.request.remote_addr,flask.request.cookies.get("token")):
+    log(flask.request.method,getIP(flask.request),flask.request.remote_addr)
+    if not validateToken(
+        flask.request.cookies.get("username", ""),
+        flask.request.remote_addr,
+        flask.request.cookies.get("token"),
+    ):
         return flask.redirect("/login")
     return flask.send_from_directory("static", "forgot.html")
 
+
 @app.route("/logout")
 def logout():
-    log(flask.request.method,flask.request.full_path,flask.request.remote_addr)
+    log(flask.request.method,getIP(flask.request),flask.request.remote_addr)
     resp = flask.make_response(flask.redirect("/"))
     resp.set_cookie("username", "", expires=0)
     resp.set_cookie("token", "", expires=0)
     return resp
 
+
 @app.route("/api", methods=["POST"])
 def api():
-    if flask.request.form.get("ping","false")=="true":
+    if flask.request.form.get("ping", "false") == "true":
         return "1"
     return ""
+
 
 @app.route("/address/<address>")
 def address(address):
     return serve_qr(address)
 
+
 @app.route("/invoice/<invoiceType>")
 def invoice(invoiceType):
-    log(flask.request.method,flask.request.full_path+" "+invoiceType,flask.request.remote_addr)
-    invoiceType=invoiceType.split("-")
+    log(
+        flask.request.method,
+        flask.request.full_path + " " + invoiceType,
+        flask.request.remote_addr,
+    )
+    invoiceType = invoiceType.split("-")
     if (invoiceType[0] in getAddress().keys()) and (invoiceType[1] in list("1234")):
-        with open("static/invoice.html","r") as f:
-            invoiceHTML=f.read()
-        amount=getAmount(invoiceType[0],invoiceType[1])
-        invoiceHTML=invoiceHTML % (str(amount),invoiceType[0],getAddress()[invoiceType[0]],getAddress()[invoiceType[0]])
+        with open("static/invoice.html", "r") as f:
+            invoiceHTML = f.read()
+        amount = getAmount(invoiceType[0], invoiceType[1])
+        invoiceHTML = invoiceHTML % (
+            str(amount),
+            invoiceType[0],
+            getAddress()[invoiceType[0]],
+            getAddress()[invoiceType[0]],
+        )
         return invoiceHTML
     return flask.redirect("/logout")
 
+
 @app.route("/success", methods=["POST"])
 def success():
-    if not validateToken(flask.request.cookies.get("username",""),flask.request.remote_addr,flask.request.cookies.get("token")):
+    if not validateToken(
+        flask.request.cookies.get("username", ""),
+        getIP(flask.request),
+        flask.request.cookies.get("token"),
+    ):
         return flask.redirect("/login")
     addTxid(flask.request.form["txid"])
-    log("INFO","Transaction Successful, txid: "+flask.request.form["txid"],flask.request.remote_addr)
-    logTxid(flask.request.form["txid"],flask.request.remote_addr)
+    log(
+        "INFO",
+        "Transaction Successful, txid: " + flask.request.form["txid"],
+        flask.request.remote_addr,
+    )
+    logTxid(flask.request.form["txid"], flask.request.remote_addr)
     return flask.send_from_directory("static", "success.html")
+
 
 # @app.route("/webhooktest/<msg>")
 # def webhooktest(msg):
